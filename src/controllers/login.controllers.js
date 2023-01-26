3c39ddf9e1d980eda3d9f499abf0068686d90b0b
Felipe Pinto <felipe14062000@gmail.com>
2023-01-26 17:14:13 -0300

    feat: criação da utils do jwt

---
+++ src/controllers/login.controllers.js
@@ -0,0 +1,7 @@
+const loginAuth = (_req, _res) => {
+  // const { email, password } = req.body;
+};
+
+module.exports = {
+  loginAuth,
+};
---
+++ src/utils/jwt.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
+const config = {
+  expiresIn: '1h',
+  algorithm: 'HS256',
+};
+
+const generateToken = ({ email }) => jwt.sign({ email }, JWT_SECRET, config);
+
+const verificationToken = (token) => {
+  try {
+    const user = jwt.verify(token, JWT_SECRET);
+    return { type: null, message: user };
+  } catch (error) {
+    return { type: 400, message: 'invalid token' };
+  }
+};
+
+module.exports = {
+  generateToken,
+  verificationToken,
+};
