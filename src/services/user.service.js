f5d1c91964d1d10e882931ef2aa2b4116c8e4b3f
Felipe Pinto <felipe14062000@gmail.com>
2023-01-26 19:03:07 -0300

    feat: criação do user service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,23 @@
-// const { User } = require('../models');
+const { Op } = require('sequelize');
+const { User } = require('../models');
+const { validateFieldsUser } = require('./validators/validatorFields');
+const { generateToken } = require('../utils/jwt');
 
-const create = () => {};
+const create = async (newUser) => {
+  const error = validateFieldsUser(newUser);
+  if (error.type) return error;
+  const existentUser = await User.findOne({
+    where: {
+      email: {
+        [Op.like]: newUser.email,
+      },
+    },
+  });
+  if (existentUser) return { type: 409, message: 'User already registered' };
+  await User.create(newUser);
+  const token = generateToken(newUser);
+  return { type: null, message: token };
+};
 
 module.exports = {
   create,
