02ece9111002953a1ee80a043d9286cf6b5461ab
Felipe Pinto <felipe14062000@gmail.com>
2023-01-27 14:19:48 -0300

    feat: criação da model Category

---
+++ src/models/Category.js
@@ -0,0 +1,14 @@
+const categorySchema = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    name: DataTypes.STRING
+  }, {
+    tableName: 'categories',
+    underscored: true,
+    timestamps: false,
+  })
+
+  return Category;
+}
+
+module.exports = categorySchema;
